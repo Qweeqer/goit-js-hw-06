e22044a5c5b11c5ba6dfc6a639177b3666ba25ae
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-06-28 21:31:13 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const allIngredients = document.querySelector("ul#ingredients");
+const addIngredient = ingredients.map(newIngredient => {
+  const nameEl = document.createElement("li")
+  nameEl.textContent = newIngredient;
+  nameEl.classList.add(`item`);
+       return nameEl;
+});
+console.log(addIngredient);
+allIngredients.append(...addIngredient);
