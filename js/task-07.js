8b1870b113ce96b04f2ae9d74c91ae72445a3485
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-06-29 21:30:20 +0300

    task 7

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+const selectFontSizeEl = document.querySelector("#font-size-control");
+const spanText = document.querySelector("#text");
+input.setAttribute.value = "16"
+selectFontSizeEl.addEventListener("input",
+    handSelectFontSize);
+    
+function handSelectFontSize() {
+    const fontSize = selectFontSizeEl.value;
+    spanText.style.fontSize = `${fontSize}px`;
+    console.log(spanText.style.fontSize);
+}
