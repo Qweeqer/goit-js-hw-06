2355ec0a27c0c4e760711d8aabc3edb0164dd0e0
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-06-29 18:34:00 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,22 +1,9 @@
-// let input = document.querySelector("#name-input");
-// let nameOutput = document.querySelector("#name-output");
-
-// input.oninput = function () {
-//     if (input.value === '') {
-//        nameOutput.innerHTML = 'Anonymous';
-//     } else{
-//     nameOutput.innerHTML = input.value;
-//     }
-// }
-// const textInput = document.querySelector(".text-input");
-// const output = document.querySelector(".output");
-
-// textInput.addEventListener("input", (event) => {
-//   output.textContent = event.currentTarget.value;
-// });
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
+const input = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+input.addEventListener("input", (event) => {
+  console.log(event.currentTarget.value);
+   if (event.currentTarget.value.trim() === '') {
+       return nameOutput.innerHTML = 'Anonymous';
+    } 
+  nameOutput.textContent = event.currentTarget.value;
 });
