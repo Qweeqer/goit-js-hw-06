65f6f2e2f84681b335f6cab078a812cb2ab26a74
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-06-30 15:24:51 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const formRef = document.querySelector(".login-form");
+formRef.addEventListener("submit", handleSubmit);
 
+const handleSubmit = (event) => {
+    event.preventDefault()
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+    return alert(`Warning that all fields must be filled!`)
+  } 
+};
+console.log(formRef.textContent);
